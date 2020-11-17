51cae8de44adf164bb362e37a9f53cfe28f3ea40
Shameel Fazul <shameel.fazul@mail.com>
2020-11-17 17:54:10 +0530

    Validation, Hooks & Hashing
    - Mongoose Validation Messages
    - Mongoose Hooks (Before/After) Save
    - Hashing passwords using bcrypt before saving to MongoDB

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,27 @@
 const User = require('../models/User');
 
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' }
+
+    // duplicate error code
+    if (err.code === 11000) { // Duplicate error code
+        errors.email = 'That email is already registered';
+        return errors; // Return if this condition is met
+    }
+
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => { // Destructuring 'properties' object from parent object
+            errors[properties.path] = properties.message; // Updating the error object
+        }) 
+        // Object.values() > Only outputs/logs object values; { }
+        // In this case, only the errors object, which is an array so we can use the forEach method.
+    }
+    return errors;
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -16,8 +38,8 @@ module.exports.signup_post = async (req, res) => {
         res.status(201).json(user);
     }
     catch (err) {
-        console.log(err);
-        res.status(400).send('error, user not created');
+        const error = handleErrors(err);
+        res.status(400).json({ error })
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -1,18 +1,40 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator'); // Package for validating emails/passwords etc.
+const bcrypt = require('bcrypt'); // Package for password hashing.
 
 const userSchema = mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'], // Second Array value is for error messages
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email'] // Triggers the function and passes the value to it to check if it's a valid email.
     },
     password: {
         type: String,
-        required: true,
-        minLength: 6,
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum passsword length is 6 characters'],
     },
 });
+
+// MONGOOSE HOOKS
+
+// fire a function after doc saved to db
+userSchema.post('save', (doc, next) => {
+    console.log('new user has been created', doc);
+    next(); // [middleware] To continue with the process or current process will be paused and there won't be a response.
+});
+
+// fire a function before doc saved to db
+userSchema.pre('save', function (next) {
+    const salt = bcrypt.genSaltSync();
+    this.password = bcrypt.hashSync(this.password, salt);
+    next();
+    // When creating a user using User.create(), it creates a new instance of the user.
+    // ^ We can access this local instance using the 'this' syntax ^
+    // ^ 'this' syntax can't be accessed in a arrow function ^
+});
+
 
 const User = mongoose.model('user', userSchema);
 
