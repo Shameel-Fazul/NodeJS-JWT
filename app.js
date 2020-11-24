3d693c84125a140a0bda22ff1f0672a5b9d0802f
Shameel Fazul <shameel.fazul@mail.com>
2020-11-24 17:10:42 +0530

    Logout / User Rendering
    BACK END
    - Logging Users Out
    - Rendering User Data on every request via JWT
    
    FRONT END
    - Added a logout button
    - User details displayed on Navbar

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const CookieParser = require('cookie-parser');
-const { requireAuth } = require('./middleware/authMiddleware');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(CookieParser()); // For parsing cookie data from the browser.
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4';
+const dbURI = '';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => {
     console.log('APP > Connected to MongoDB');
@@ -24,6 +24,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser); // apply Middleware to every GET request
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
+// require auth
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
 
@@ -19,4 +21,30 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-module.exports = { requireAuth }
+// check user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, 'shameel secret code', async (err, decodedToken) => {
+           if (err) {
+               console.log(err.message);
+               res.locals.user = null;
+               next();
+           } else {
+               console.log(decodedToken);
+               let user = await User.findById(decodedToken.id);
+               //res.locals.`variablename` = data  // Locals are used to declare variable that can be accessed in the view
+               res.locals.user = user;
+               console.log(user);
+               next();
+           }
+        })
+    }
+    else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser }
